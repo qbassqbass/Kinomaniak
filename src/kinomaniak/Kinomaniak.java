f23667ecd24b6d15f2886e02d544786cbd0a3915
Jakub Pałac <jakub.palac@gmail.com>
2013-03-22 00:26:21 +0100

    changed hierarchy of classes

--- src/kinomaniak/Kinomaniak.java
+++ src/kinomaniak/Kinomaniak.java
@@ -14,6 +14,6 @@ public class Kinomaniak {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Kinomaniak");
     }
 }
